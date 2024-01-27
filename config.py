db7a4eb664af08a17630ba81c06db2e0f50aa41b
Mopsyatin <mopsyatin@gmail.com>
2024-01-27 03:40:25 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,1 @@
+token = "6474671912:AAFI8Acm_4wk7o0U3-DFw3uexU4bgCYcXdY"
---
+++ logic.py
@@ -0,0 +1,70 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.type = self.get_type()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']["other"]['official-artwork']["front_default"])
+        else:
+            return "https://i.pinimg.com/originals/bf/95/34/bf953419d76bf747cba69b55e6e03957.png"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+    
+    def get_type(self):
+        list = []
+        string = ''
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+
+            for i in range(len(data['types'])):
+                list.append(data['types'][i]['type']['name'])
+
+            for l in list:
+                string += l
+
+            return(string)
+            
+        else:
+            return "Electric"
+
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покеомона: {self.name}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+    
+    def type_info(self):
+        return f"Типы твоего покемона: {self.type}"
+
+
+
